bcecd1fddb33ee3e6ee48ed37eb74e04ce0f5dd5
coconut <coconut02222@gmail.com>
2023-09-01 16:46:50 +0900

    오류수정

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -3,16 +3,17 @@ import { getProducts } from '../api/firebase'
 import axios from 'axios'
 
 export default function useProducts() {
-    const [allProducts, setAllProducts]=useState([])
-  
-    useEffect(()=>{
-      axios.get('/data/products.json').then((res)=>{
-        setAllProducts(res.data)
-      // getProducts().then((res)=>{
-      //   setAllProducts(res)
-      // })
-      },)
-    },[])
+  const [allProducts, setAllProducts]=useState([])
+
+  useEffect(()=>{
+    // axios.get('/data/products.json').then((res)=>{
+    //   setAllProducts(res.data)
+    // })
+    getProducts().then((res)=>{
+      setAllProducts(res)
+    })
+  },[])
+
   return (
     [allProducts]
   )
--- src/pages/Productdetail.js
+++ src/pages/Productdetail.js
@@ -1,59 +1,73 @@
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import styles from '../component/styles/productdetail.module.css'
 import { RiHeartAddLine } from 'react-icons/ri'
 import { useEffect, useState } from 'react';
-import useProducts from '../hooks/useProducts'
-// import { getProductDetail } from '../api/firebase'
+// import useProducts from '../hooks/useProducts'
+import { getProductDetail } from '../api/firebase'
 // import axios from 'axios'
 
 export default function Productdetail() {
     const {productId}=useParams();
-    const [allProducts]=useProducts();
+    // const [allProducts]=useProducts();
     const [productItem, setProductItem]=useState([]);
+    // useEffect(()=>{
+    //     const productItem=allProducts.filter((item)=>item.id===productId)
+    //     setProductItem(productItem)
+    // },[productId,allProducts])
+    const {pathname}= useLocation();
     useEffect(()=>{
-        const productItem=allProducts.filter((item)=>item.id===productId)
-        setProductItem(productItem)
-    },[productId,allProducts])
-    // useEffect(() => {
-    //   getProductDetail(productId).then((res)=>{
-    //     setProductItem(res)
-    //   })
-    // }, [productId])
-   
+      window.scrollTo(0,0);
+    }, [pathname])
+
+    useEffect(() => {
+      getProductDetail(productId).then((res)=>{
+        // setProductItem(res)
+        if (Array.isArray(res)) {
+          setProductItem(res);
+        } else if (res) {
+          setProductItem([res]);
+        } else {
+          console.log('Product not found');
+        }
+      })
+    }, [productId])
+
   return (
     <>
         {/* <div>
             <input type="text" onChange={changeId}/><button onClick={submitId}><Link to=''>상세보기</Link></button>
         </div> */}
-        {
-    productItem.map((item)=>(
-       <section className={styles.productdetail} key={item.id}>
-        <div className={styles.productdetail_box}>
-        <div className={styles.productdetail_img}>
-            <img src={item.image} alt={item.name}/>
-        </div>
-        <div className={styles.productdetail_text_box}>
-            <p className={styles.productdetail_name}>{item.name}</p>
-            <ul className={styles.selectcolor}>
-            {item.colors.map((color, index) => (
-                    <li key={index}>
-                      <button>{color}</button>
-                    </li>
-                  ))}
-            </ul>
-            <p className={styles.productdetail_text}>
-            {item.text} 
-            </p>
-            <p className={styles.productdetail_price}>$ {item.price}</p>
-            <ul className={styles.productdetail_button}>
+      {
+      productItem.map((item)=>(
+        <section className={styles.productdetail} key={item.id}>
+          <div className={styles.productdetail_box}>
+            <div className={styles.productdetail_img}>
+                <img src={item.image} alt={item.name}/>
+            </div>
+            <div className={styles.productdetail_text_box}>
+              <p className={styles.productdetail_name}>{item.name}</p>
+              <ul className={styles.selectcolor}>
+              {
+                item.colors.map((color, index) => (
+                  <li key={index}>
+                    <button>{color}</button>
+                  </li>
+                ))
+              }
+              </ul>
+              <p className={styles.productdetail_text}>
+              {item.text} 
+              </p>
+              <p className={styles.productdetail_price}>$ {item.price}</p>
+              <ul className={styles.productdetail_button}>
                 <li><button><RiHeartAddLine/></button></li>
                 <li><button>BUY</button></li>
                 <li><button>ADD TO CART</button></li>
-            </ul>
-        </div>
-        </div>
-       </section>
-       ))
+              </ul>
+            </div>
+          </div>
+        </section>
+        ))
        }
     </>
   )
